59748d0beffab8c6df71c19fdff42ec0d5df5716
kenmckaba <ken@mckaba.com>
2025-04-27 21:28:34 -0700

    fix build error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,7 @@
 import '@/app/ui/global.css'
 
-import { Inter, Lusitana } from 'next/font/google'
+import { Lusitana } from 'next/font/google'
 
-export const inter = Inter({ subsets: ['latin'] })
-
-export const lusitana = Lusitana({
-  weight: ['400', '700'],
-  subsets: ['latin'],
-})
 export default function RootLayout({
   children,
 }: {
